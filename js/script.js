95dfff4fb4eb114b0bccb1ad22b818a3fd26e401
Miftahuddin-cod <miftahuddin.cod@gmail.com>
2021-11-26 21:53:05 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// sidenav open
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+}
+
+// sidenav close
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+}
